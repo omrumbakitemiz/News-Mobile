5d32a3f37795e827a854fca7d3274f68201d9c18
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-04-14 16:02:59 +0300

    news module

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,13 +5,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
+import { MatButtonModule, MatInputModule } from '@angular/material';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    MatButtonModule,
+    MatInputModule,
+    RouterModule.forChild([{path: '', component: Tab1Page}])
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+}
